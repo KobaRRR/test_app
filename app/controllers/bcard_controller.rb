e3b21143d9ed8eae9c35cfef0b1fb01a62723530
rKoba <kobayashi-r@jyohokikaku.co.jp>
2022-12-28 12:01:30 +0900

    search

--- app/controllers/bcard_controller.rb
+++ app/controllers/bcard_controller.rb
@@ -6,21 +6,33 @@ class BcardController < ApplicationController
   def index
     #めちゃくちゃ見た目ごちゃっちゃった
     @ddlBCards = BusinessCard.select(:company_name).group(:company_name)
-    # @ddlValue = BusinessCard.find_by(company_name: params[:company_name])
-    #DDL選択
-    if params[:company_name]
-      if params[:company_name] == 'all'
-        @BCards = BusinessCard.all
-      elsif params[:company_name] == 'default'
-       @BCards = BusinessCard.where(user_id: @current_user.user_id)
-      else
-        @BCards = BusinessCard.where(user_id: @current_user.user_id, company_name: params[:company_name])
+
+    sqlQuery = "select * from business_cards where 1 = 1 "
+
+    #Sort
+      if params[:company_name]
+        if params[:company_name] == 'all'
+          #条件追加なし。
+        elsif params[:company_name] == 'default'
+          # @BCards = BusinessCard.where(user_id: @current_user.user_id)
+          sqlQuery.concat(" and user_id = #{@current_user.user_id}")
+        else
+          # @BCards = BusinessCard.where(user_id: @current_user.user_id, company_name: params[:company_name])
+          sqlQuery.concat(" and company_name = '#{params[:company_name]}'")
+        end
+
       end
 
-    else
+      if params[:name]
+        likeQuery = " and name like '%#{params[:name]}%'"
+        likeQuery.concat(" union ", sqlQuery, " and company_name like '%#{params[:name]}%'")
+        sqlQuery.concat(likeQuery)
     #DDL未選択
-      @BCards = BusinessCard.where(user_id: @current_user.user_id)
-    end
+      # @BCards = BusinessCard.where(user_id: @current_user.user_id)
+      end
+      
+    @BCards = BusinessCard.find_by_sql(sqlQuery)
+
   end
 
   def new
